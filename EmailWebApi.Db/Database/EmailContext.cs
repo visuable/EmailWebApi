eeacc2800f8f182d85cca18514e93f04d58fe41f
Nikita Korabelnikov <n.korabelnikov@isoit.ru>
2020-12-31 10:10:11 +0300

    Update EmailContext

--- EmailWebApi.Db/Database/EmailContext.cs
+++ EmailWebApi.Db/Database/EmailContext.cs
@@ -11,23 +11,5 @@ namespace EmailWebApi.Db.Database
         }
 
         public DbSet<Email> Emails { get; set; }
-        public DbSet<EmailState> States { get; set; }
-        public DbSet<EmailInfo> Infos { get; set; }
-        public DbSet<EmailContent> Contents { get; set; }
-
-        public DbSet<EmailBody> Bodies { get; set; }
-        //public DbSet<ThrottlingStateDto> ThrottlingStates { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-//            modelBuilder.Entity<ThrottlingStateDto>().HasData(new ThrottlingStateDto
-//            {
-//                Counter = 0,
-//                EndPoint = DateTime.Now.AddSeconds(60),
-//                LastAddress = string.Empty,
-//                LastAddressCounter = 0,
-//                Id = 1
-//            });
-        }
     }
 }
